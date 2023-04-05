346131bf15470e3a73f83fbdf2ab7cb07edcbe14
Michael Bianco <iloveitaly@gmail.com>
2023-04-05 11:24:46 -0600

    adding project todoist option

--- clean_workspace/__init__.py
+++ clean_workspace/__init__.py
@@ -6,12 +6,7 @@ import typing as t
 from ScriptingBridge import SBApplication
 import chrome_bookmarks
 
-def export_to_todoist(task_content, description):
-    # TODO should also support .env here as well
-    import dotenv
-
-    dotenv.load_dotenv(".envrc")
-
+def todoist_client():
     # extract todoist api key from environment without throwing an exception
     todoist_api_key = os.environ.get("TODOIST_API_KEY", None)
 
@@ -21,21 +16,41 @@ def export_to_todoist(task_content, description):
 
     print("todoist api key found, adding to todoist")
 
-    import datetime
-
     from todoist_api_python.api import TodoistAPI
 
     api = TodoistAPI(todoist_api_key)
+    return api
+
+def export_to_todoist(task_content, description):
+    # TODO should also support .env here as well
+    import dotenv
+    dotenv.load_dotenv(".envrc")
+
+    api = todoist_client()
+    if not api:
+        return
+
+    import datetime
+
+    project_name = os.environ.get("TODOIST_PROJECT", "Learning")
+    project = None
+    projects = api.get_projects()
+    project_matches = [project for project in projects if project.name == project_name]
+
+    if len(project_matches) == 1:
+        project = project_matches[0]
+
 
     # find a label called "web-archive" or create it
+    label_name = os.environ.get("TODOIST_LABEL", "web-archive")
     labels = api.get_labels()
-    label_matches = [label for label in labels if label.name == "web-archive"]
+    label_matches = [label for label in labels if label.name == label_name]
 
     # assigning label for debugging
     if len(label_matches) == 0:
         # https://developer.todoist.com/rest/v1/#create-a-new-label
-        print("could not find web-archive label, creating it")
-        label = api.add_label(name="web-archive")
+        print(f"could not find {label_name} label, creating it")
+        label = api.add_label(name=label_name)
     else:
         label = label_matches[0]
 
@@ -47,7 +62,8 @@ def export_to_todoist(task_content, description):
         ),
         description=task_content,
         due_string="today",
-        labels=["web-archive"],
+        labels=[label_name],
+        project_id=project.id if project else None,
     )
 
 def get_browser_urls() -> t.List[str]:
@@ -114,7 +130,7 @@ def main():
     # strip all anchors from the urls
     browser_urls = [(url.split("#")[0], name) for url, name in browser_urls]
 
-
+    # TODO load these files from a home config file
 
     # user configurable blacklist for urls you don't want to archive
     url_blacklist = []
@@ -132,7 +148,7 @@ def main():
     bookmark_urls = get_bookmarks_urls()
 
     # TODO output skipped domains
-    # TODO support wildcard subdomains
+    # TODO support wildcard subdomains, *.sentry.io
     # filter all urls with blacklisted domains
     browser_urls = [x for x in browser_urls if x[0].split("/")[2] not in domain_blacklist]
 
@@ -140,7 +156,10 @@ def main():
     todoist_content = ""
     for url_with_name in browser_urls:
         if (
+            # if the url is in the bookmark list of chrome or safari, skip it
             url_with_name[0] not in bookmark_urls
+            # TODO should allow for regex in the URL matching, or at least globbing
+            # if the url
             and url_with_name[0] not in url_blacklist
         ):
             todoist_content += "* " + " - ".join(url_with_name) + "\n"
