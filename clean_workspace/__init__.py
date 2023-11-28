d2b6f0b1f6e60dd0cb60b17910587d97a6ec90d6
Michael Bianco <iloveitaly@gmail.com>
2023-11-28 09:14:38 -0700

    fix: adding backoff to all todoist calls

--- clean_workspace/__init__.py
+++ clean_workspace/__init__.py
@@ -12,6 +12,37 @@ from ScriptingBridge import SBApplication
 from todoist_api_python.api import TodoistAPI
 
 
+# https://github.com/Doist/todoist-api-python/issues/38
+# backoff 5xx errors
+def patch_todoist_api():
+    import backoff
+    import requests
+    import todoist_api_python.http_requests
+
+    patch_targets = ["delete", "get", "json", "post"]
+    for target in patch_targets:
+        original_function = getattr(todoist_api_python.http_requests, target)
+
+        setattr(
+            todoist_api_python.http_requests,
+            f"original_{target}",
+            original_function,
+        )
+
+        patched_function = backoff.on_exception(
+            backoff.expo, requests.exceptions.HTTPError
+        )(original_function)
+
+        setattr(
+            todoist_api_python.http_requests,
+            target,
+            patched_function,
+        )
+
+
+patch_todoist_api()
+
+
 def _todoist_api_key():
     return os.environ.get("TODOIST_API_KEY", None)
 
